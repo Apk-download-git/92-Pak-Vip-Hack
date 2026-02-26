f2792f9b54b4386509953733602f898b45d47370
Rishav <techymind108@gmail.com>
2026-02-26 12:39:07 +0000

    Delete download.php

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = "counter.txt";
-$count = file_exists($file) ? file_get_contents($file) : 0;
-$count++;
-file_put_contents($file, $count);
-
-header("Location: Application/92Pak Vip Hack.apk");
-exit();
-?>
